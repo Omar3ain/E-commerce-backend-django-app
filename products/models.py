ea218060d543326bc49cf2384e41e3b404d561eb
omar3ain <omar3ain@gmail.com>
2023-04-30 22:31:30 +0300

    added product and category models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from shared.models import Category
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image_urls = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,8 @@
+# serializers.py
 from rest_framework import serializers
+from .models import Product
 
-
-class productSerializer(serializers.ModelSerializer):
-    pass
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
