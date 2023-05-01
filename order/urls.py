edc3ca2bff8b8f648372edb52e436a7597036453
omar3ain <omar3ain@gmail.com>
2023-05-01 22:00:12 +0300

    first desgin for routes of project (user , wishlist , order and payments)

--- order/urls.py
+++ order/urls.py
@@ -1,3 +1,5 @@
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = []
+urlpatterns = [
+  path('payment/', include('payments.urls')),
+]
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 from .views import CreateUserView, LoginView
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create_user'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('cart/', include('cart.urls')),
+    path('order/', include('order.urls')),
+    path('wishlist/', include('wishlist.urls')),
 ]
