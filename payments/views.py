d5b6340241860e31d26f8190446705bc90d52d3d
hayah1999 <hayahhazem1999@gmail.com>
2023-05-04 15:41:01 +0300

    add payment order check

--- payments/views.py
+++ payments/views.py
@@ -24,23 +24,26 @@ class CreatePayment(APIView):
         self.check_object_permissions(request, user)
         try:
             order = Order.objects.get(id=orderId)
-            intent = stripe.PaymentIntent.create(
-                amount = order.total_amount,
-                currency = 'USD',
-                automatic_payment_methods={
-                'enabled': True,
-                },
-                metadata = {
-                    'orderId' : orderId,
-                    'userId' : user.id,
-                },
-                receipt_email = request.user.email
-            )
-            order.paid = True
-            order.save()
-            payment = Payment.objects.create( amount= intent.amount, currency= intent.currency, stripe_charge_id= intent.id, order= order )
-            serializer = paymentSerializer(payment)
-            return Response({'clientSecret' : intent['client_secret'], 'stripe-payment': intent, 'data' : serializer.data }, status= status.HTTP_200_OK)
+            if not order.paid:
+                intent = stripe.PaymentIntent.create(
+                    amount = order.total_amount,
+                    currency = 'USD',
+                    automatic_payment_methods={
+                    'enabled': True,
+                    },
+                    metadata = {
+                        'orderId' : orderId,
+                        'userId' : user.id,
+                    },
+                    receipt_email = request.user.email
+                )
+                order.paid = True
+                order.save()
+                payment = Payment.objects.create( amount= intent.amount, currency= intent.currency, stripe_charge_id= intent.id, order= order )
+                serializer = paymentSerializer(payment)
+                return Response({'clientSecret' : intent['client_secret'], 'stripe-payment': intent, 'data' : serializer.data }, status= status.HTTP_200_OK)
+            else:
+                return Response({'error' :  "Order already paid"})
         except Exception as e:
             return Response({'error' : str(e)})
         except Order.DoesNotExist:
