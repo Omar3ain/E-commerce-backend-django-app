197b466077a4c3a8d746facf2f8026f2c7806366
hayah1999 <hayahhazem1999@gmail.com>
2023-05-10 16:37:34 +0300

    modify cart functions

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,6 +1,7 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Cart, CartItem
+from products.serializers import ProductSerializer
 
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,6 +9,7 @@ class CartSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CartItemSerializer(serializers.ModelSerializer):
+    product_id = ProductSerializer(read_only=True);
     class Meta:
         model = CartItem
-        fields = '__all__'
+        fields = ('id', 'product_id', 'quantity', 'cart_id')
--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CartCreateView, GetCart, DeleteCartItem
+from .views import CartCreateView, GetCart, DeleteCartItem, AddQuantity
 
 urlpatterns = [
        path('', GetCart.as_view(), name='cart-detail'),
        path('<int:product_id>/', CartCreateView.as_view(), name='cart-add'),
        path('delete/<int:cartitem>/', DeleteCartItem.as_view(), name='cartItem-delete'),
+       path('add/<int:cartitem>',  AddQuantity.as_view(), name='cartItem-add'),
 ]
