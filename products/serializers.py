78f3a8b51d627b60c3020320c81e268178c93278
somayaax <somayaa192@gmail.com>
2023-05-14 22:07:50 +0300

    check in wishlist

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,16 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Product
-
+from wishlist.models import Wishlist
 class ProductSerializer(serializers.ModelSerializer):
+    inWishList = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_inWishList(self, obj):
+        request = self.context.get('request')
+        if request and request.user:
+            return Wishlist.objects.filter(product=obj.id, user_id = request.user.id).exists()
+        else:
+            return False  
