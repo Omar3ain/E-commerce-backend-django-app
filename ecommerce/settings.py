3bfa1c8a914334ddbd86faa95e845b08d79c877a
somayaax <somayaa192@gmail.com>
2023-05-18 18:43:00 +0300

    add pagination and search

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -163,6 +163,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
 }
 
 AUTH_USER_MODEL = 'users.User'
