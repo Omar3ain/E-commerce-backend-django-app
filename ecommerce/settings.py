ae3a7f514573e172775110dd484ee729241d1111
muhannad ibrahim <muhannad.ibrahim2311@gmail.com>
2023-05-20 22:12:37 +0300

    change url client side

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['sellio-ecommerce-app.onrender.com', 'https://sellio-ecommerce.onrender.com']
+ALLOWED_HOSTS = ['sellio-ecommerce-app.onrender.com']
 
 
 # Application definition
@@ -79,6 +79,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173',  # Replace with your frontend domain and port
+    'https://sellio-ecommerce.onrender.com',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
